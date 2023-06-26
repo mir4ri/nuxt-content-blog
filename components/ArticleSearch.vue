0ad2b07e525534913ab492d6b1cc9187ebdde2e2
mir4ri <leobuchko@gmail.com>
2023-06-26 12:01:34 +0300

    feat: added search

---
+++ components/ArticleSearch.vue
@@ -0,0 +1,16 @@
+<!-- ./components/Search.vue -->
+
+<script setup>
+  let search = '';
+</script>
+
+<template>
+  <div class="search">
+    <input
+      type="text"
+      placeholder="Пошук"
+      class="search-input"
+      @input="search"
+    />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,74 +2,102 @@
 
 <script setup>
 definePageMeta({
-  key: (route) => route.fullPath,
+	key: (route) => route.fullPath,
 });
 
 // get tag query
 const {
-  query: { tags },
+	query: { tags },
 } = useRoute();
 
 const filter = ref(tags?.split(","));
 
+const query = ref("");
+console.log(filter, query);
 // set meta for page
 useHead({
-  title: "All articles",
-  meta: [{ name: "description", content: "Тут ви знайдете всі написані статті" }],
+	title: "All articles",
+	meta: [
+		{ name: "description", content: "Тут ви знайдете всі написані статті" },
+	],
 });
 </script>
 <template>
-  <main>
-    <header class="page-heading">
-      <div class="wrapper">
-        <h1 class="text-5xl font-extrabold">Всі статті</h1>
-        <p class="font-medium text-lg">Тут ви знайдете всі написані статті</p>
-      </div>
-    </header>
-    <section class="page-section">
-      <Tags />
+	<main>
+		<header class="page-heading">
+			<div class="wrapper">
+				<h1 class="text-5xl font-extrabold">Всі статті</h1>
+				<p class="font-medium text-lg">Тут ви знайдете всі написані статті</p>
+			</div>
+		</header>
+		<section class="page-section">
+			<input
+				id="search-blogs"
+				v-model="query"
+				class="px-3 py-2 border border-slate-200 rounded-lg w-full mb-2"
+				type="text"
+				placeholder="Пошук по статтям"
+			/>
 
-      <!-- Render list of all articles in ./content/blog using `path` -->
-      <!-- Provide only defined fieldsin the `:query` prop -->
-      <ContentList
-        path="/blog"
-        :query="{
-          only: ['title', 'description', 'tags', '_path', 'img'],
-          where: {
-            tags: {
-              $contains: filter,
-            },
-          },
-          $sensitivity: 'base',
-        }"
-      >
-        <!-- Default list slot -->
-        <template v-slot="{ list }">
-          <ul class="article-list">
-            <li v-for="article in list" :key="article._path" class="article-item">
-              <NuxtLink :to="article._path">
-                <div class="wrapper">
-                  <div class="img-cont w-32 shrink-0">
-                    <img :src="`/${article.img}`" :alt="article.title" class="rounded-lg max-h-[8rem]" />
-                  </div>
-                  <header>
-                    <h1 class="text-2xl font-semibold">{{ article.title }}</h1>
-                    <p>{{ article.description }}</p>
-                    <ul class="article-tags">
-                      <li class="tag !py-0.5" v-for="(tag, n) in article.tags" :key="n">{{ tag }}</li>
-                    </ul>
-                  </header>
-                </div>
-              </NuxtLink>
-            </li>
-          </ul>
-        </template>
+			<Tags />
+			<!-- Render list of all articles in ./content/blog using `path` -->
+			<!-- Provide only defined fieldsin the `:query` prop -->
+			<ContentList
+				path="/blog"
+				:query="{
+					only: ['title', 'description', 'tags', '_path', 'img'],
+					where: {
+						tags: {
+							$contains: filter,
+						},
+						title: {
+							$contains: query,
+						},
+					},
+					$sensitivity: 'base',
+				}"
+			>
+				<!-- Default list slot -->
+				<template v-slot="{ list }">
+					<ul class="article-list">
+						<li
+							v-for="article in list"
+							:key="article._path"
+							class="article-item"
+						>
+							<NuxtLink :to="article._path">
+								<div class="wrapper">
+									<div class="img-cont w-32 shrink-0">
+										<img
+											:src="`/${article.img}`"
+											:alt="article.title"
+											class="rounded-lg max-h-[8rem]"
+										/>
+									</div>
+									<header>
+										<h1 class="text-2xl font-semibold">{{ article.title }}</h1>
+										<p>{{ article.description }}</p>
+										<ul class="article-tags">
+											<li
+												class="tag !py-0.5"
+												v-for="(tag, n) in article.tags"
+												:key="n"
+											>
+												{{ tag }}
+											</li>
+										</ul>
+									</header>
+								</div>
+							</NuxtLink>
+						</li>
+					</ul>
+				</template>
 
-        <!-- Not found slot to display message when no content us is found -->
-        <template #not-found>
-          <p>Статтів не знайдено.</p>
-        </template>
-      </ContentList>
-    </section>
-  </main>
+				<!-- Not found slot to display message when no content us is found -->
+				<template #not-found>
+					<p>Статтів не знайдено.</p>
+				</template>
+			</ContentList>
+		</section>
+	</main>
 </template>
